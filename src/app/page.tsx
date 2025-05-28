0102c246661503857200e9181aa52138f7703d4b
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-05-28 08:39:08 +0300

    v1.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import HeroSection from "@/components/Hero/Hero";
 import HeroSectionTest from "@/components/Hero/HeroTest";
-import ProgramSection from "@/components/Program/ProgramSection";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
       <HeroSectionTest />
-      <ProgramSection />
+      {/* <ProgramSection /> */}
     </div>
   );
 }
