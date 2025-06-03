35f8e61e8bd75a92815af540d6be033a23fdbadc
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2025-06-03 11:27:51 +0300

    v3.9

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,19 +5,22 @@ import { authReducer } from "./features/authSlice";
 import { authAPI } from "./api/authApi";
 import { candidateAPI } from "./api/candidateApi";
 import { usersAPI } from "./api/userApi";
+import { recruiterAPI } from "./api/recruiterApi";
 
 export const store = configureStore({
   reducer: {
     authReducer,
     [authAPI.reducerPath]: authAPI.reducer,
     [candidateAPI.reducerPath]: candidateAPI.reducer,
+    [recruiterAPI.reducerPath]: recruiterAPI.reducer,
     [usersAPI.reducerPath]: usersAPI.reducer,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(
       authAPI.middleware,
       candidateAPI.middleware,
-      usersAPI.middleware
+      usersAPI.middleware,
+      recruiterAPI.middleware
     );
   },
 });
